ce27c0caebdba02df5ac58133bbf3fdf4da9b65a
awatson32 <audrey.watson32@gmail.com>
2016-03-17 10:56:48 -0500

    .then use

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,13 +1,22 @@
 var controllers = angular.module('myApp.controllers', []);
 //controllers go here
-controllers.controller('MainController', ['$scope', function($scope) {
+controllers.controller('MainController', ['$scope', '$location', function($scope, $location) {
     $scope.welcomeMessage = 'Hello World!';
     $scope.login = function() {
         console.log('Logging in...');
+        $location.path('/tweets');
     }  
 }]);    
 controllers.controller('TweetController', ['$scope', '$http', function($scope, $http) {
-    $http.get('/messages').success(function(data) {
-        $scope.messages = data;
-    });
+    $scope.getTweets = function() {  
+        $http({
+            method: 'GET',
+            url: '/messages'
+        }).then(function success(data) {
+            $scope.tweets = data;
+        }, function error(data) {
+            alert('Cannot load Tweets');
+        });
+    }
+  
 }]);
