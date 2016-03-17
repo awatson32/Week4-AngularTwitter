cec12276062a0da02f6f011c257de40ec8cb4525
awatson32 <audrey.watson32@gmail.com>
2016-03-16 19:50:16 -0500

    More functionality. Need to add bind to login button and tweet submit button

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,7 +1,10 @@
 var controllers = angular.module('myApp.controllers', []);
 //controllers go here
 controllers.controller('MainController', ['$scope', function($scope) {
-    $scope.welcomeMessage = 'Hello World!';  
+    $scope.welcomeMessage = 'Hello World!';
+    $scope.login = function() {
+        console.log('Logging in...');
+    }  
 }]);    
 controllers.controller('TweetController', ['$scope', '$http', function($scope, $http) {
     $http.get('/messages').success(function(data) {
